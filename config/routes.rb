27d159965858e39f7870fce9faf3150392cde579
hatsu38 <hajiwata0308@gmail.com>
2021-06-01 19:08:06 +0900

    Add subdomain routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
+class ExcludedSubdomainConstraint
+  def self.matches?(request)
+    request.subdomain.present? &&
+      !Apartment::Elevators::Subdomain.excluded_subdomains.include?(request.subdomain)
+  end
+end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "companies#index"
   resources :companies, only: [:index, :new, :create]
-  resources :departments, only:[:index, :new, :create]
+
+  constraints ExcludedSubdomainConstraint do
+    resources :departments, only:[:index, :new, :create]
+  end
 end
