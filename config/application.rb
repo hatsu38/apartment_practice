602fe8151c4f550567288a27d8d1db046d7bad52
hatsu38 <hajiwata0308@gmail.com>
2021-06-13 17:58:57 +0900

    Fix timezone

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,9 @@ module ApartmentPractice
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
