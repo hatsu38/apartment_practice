8ea014c6c961fedf27d095bb7a733e27b5746df7
hatsu38 <hajiwata0308@gmail.com>
2021-06-15 16:32:06 +0900

    不要なコード削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # config.hosts << "app"
-  # config.hosts << ".apartment-practice.hatsu38.com"
-  # config.hosts << "apartment-practice.hatsu38.com"
   config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
