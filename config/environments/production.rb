113a4f84bb566398244c5159856a9f33599078fa
hatsu38 <hajiwata0308@gmail.com>
2021-06-16 12:25:03 +0900

    Add asset async for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = 'http://assets.example.com'
-
+  config.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
