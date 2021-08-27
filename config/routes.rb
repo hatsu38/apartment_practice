e4abd892570c751d28bed5e96dc46379be4ca1b7
hatsu38 <hajiwata0308@gmail.com>
2021-08-27 15:14:03 +0900

    ber generate graphql:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
+
+  if Rails.env.development?
+    post "/graphql", to: "graphql#execute"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "companies#index"
   require 'sidekiq/web'
